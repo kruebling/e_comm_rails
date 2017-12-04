8046d169a3c82c5ee72bfca7e50d88d748da8629
David Hale and Keegan Ruebling <david@>
2017-12-04 09:12:23 -0800

    update devise to sign in with username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
+
+  validates :username, :presence => true
+  validates :username, :uniqueness => true
+
+  validates :password, :presence => true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
