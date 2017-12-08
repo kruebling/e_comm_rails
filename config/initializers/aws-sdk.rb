76c345dfdaa782352dac4276b8fce19630ceba15
David Hale and Keegan Ruebling <keegan@>
2017-12-07 17:36:45 -0800

    aws I2 malarkey

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,5 +1,5 @@
 S3Client = Aws::S3::Client.new(
-    access_key_id: 'AWSAccessKeyId',
-    secret_access_key: 'AWSSecretKey',
+    access_key_id: 'ACCESS_KEY_ID',
+    secret_access_key: 'SECRET_ACCESS_KEY',
     region: 'AWS_REGION'
 )
