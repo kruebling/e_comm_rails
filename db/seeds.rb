cbfbc3b24e52138a7ca50c2782ff8258c22ca966
David Hale and Keegan Ruebling <david@>
2017-12-07 16:40:41 -0800

    add env

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Product.destroy_all
 
 #seeding products
-50.times do
+1.times do
   Product.create( name: Faker::Beer.unique.name,
                   price: Faker::Number.between(5, 100),
-                  image: Faker::LoremPixel.image)
+                  image: URI.parse(Faker::LoremPixel.image))
 end
 
 p "Created #{Product.count} products"
